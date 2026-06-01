9d023e95fc472bc16966cb6bb841f291b77f39ba
kbitbi4-maker <kbitbi4@yandex.ru>
2026-06-01 11:29:58 +0700

    Update main.js

--- arifmet/js/main.js
+++ arifmet/js/main.js
@@ -7,8 +7,8 @@ function initTensMode() {
 }
 
 function generateExample() {
-    // ИСПРАВЛЕНО: Добавлена приставка window. к глобальным переменным состояния
-    if (window.currentMode !== 'tens') return;
+    // ИСПРАВЛЕНО: Теперь генератор разрешает работу как в режиме Десятков, так и в Миксе
+    if (window.currentMode !== 'tens' && window.currentMode !== 'mix') return;
     let num1, num2, correctValue, text;
     
     if (isAddition) {
@@ -16,7 +16,7 @@ function generateExample() {
             num1 = Math.floor(Math.random() * 90) + 10;
             num2 = Math.floor(Math.random() * 90) + 10;
             if ((num1 % 10 + num2 % 10) > 10 && (num1 + num2) < 100) {
-                text = `${num1}+${num2}`;
+                text = num1 + '+' + num2;
                 correctValue = num1 + num2;
                 break;
             }
@@ -26,14 +26,13 @@ function generateExample() {
             num1 = Math.floor(Math.random() * 90) + 10;
             num2 = Math.floor(Math.random() * 90) + 10;
             if (num1 > num2 && (num2 % 10) >= (num1 % 10 + 1) && (num1 - num2) >= 1) {
-                text = `${num1}-${num2}`;
+                text = num1 + '-' + num2;
                 correctValue = num1 - num2;
                 break;
             }
         }
     }
     
-    // ИСПРАВЛЕНО: Запись идет строго в window.examplesHistory
     window.examplesHistory.push({
         exampleText: text,
         correctValue: correctValue,
