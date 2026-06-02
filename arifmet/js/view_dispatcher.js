370a4949ce6a581d48dd5501ff047d84727b3bc7
kbitbi4-maker <kbitbi4@yandex.ru>
2026-06-03 06:48:59 +0700

    Update view_dispatcher.js

--- arifmet/js/view_dispatcher.js
+++ arifmet/js/view_dispatcher.js
@@ -1,1 +1,14 @@
+import { state } from './state.js';
+import { renderMonsterGame, syncMonsterGame } from './multiplication.js';
+import { renderTensVisual } from './tens.js';
+
+export function selectExample(index) {
+    state.activeIndex = index;
+    
+    if (state.currentMode === 'multiplication') {
+        syncMonsterGame();
+    } else if (state.currentMode === 'tens' || state.currentMode === 'mix') {
+        renderTensVisual();
+    }
+}
 
