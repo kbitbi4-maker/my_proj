233ea0428e1673a6be7295b419d929d4f47776c6
kbitbi4-maker <kbitbi4@yandex.ru>
2026-07-05 14:02:58 +0700

    Update main_editor.js

--- animation/js/main_editor.js
+++ animation/js/main_editor.js
@@ -1,1 +1,29 @@
+import { EditorCore } from './editor_core.js';
+import { AnimationPlayer } from './animation_player.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // 1. Инициализируем ядро рисования
+    EditorCore.init();
+
+    // 2. Инициализируем плеер кадров
+    AnimationPlayer.init();
+
+    // 3. Вешаем базовые слушатели на кнопки
+    document.getElementById('clearGridBtn').addEventListener('click', () => {
+        EditorCore.clearCurrentGrid();
+    });
+
+    document.getElementById('addFrameBtn').addEventListener('click', () => {
+        AnimationPlayer.addFrame();
+    });
+
+    document.getElementById('playBtn').addEventListener('click', () => {
+        AnimationPlayer.togglePlay();
+    });
+
+    document.getElementById('fpsSlider').addEventListener('input', (e) => {
+        document.getElementById('fpsVal').innerText = e.target.value;
+        AnimationPlayer.updateSpeed();
+    });
+});
 
