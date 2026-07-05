c4b68938162c9f16c9902581ae37f5add94cc921
kbitbi4-maker <kbitbi4@yandex.ru>
2026-07-05 14:29:01 +0700

    Update main_editor.js

--- animation/js/main_editor.js
+++ animation/js/main_editor.js
@@ -2,30 +2,35 @@ import { EditorCore } from './editor_core.js';
 import { AnimationPlayer } from './animation_player.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Безопасная инициализация модулей
-    try {
-        EditorCore.init();
-        AnimationPlayer.init();
-    } catch (error) {
-        console.error("Ошибка инициализации редактора:", error);
-    }
+    EditorCore.init();
+    AnimationPlayer.init();
 
-    // Слушатели интерфейса
-    document.getElementById('clearGridBtn')?.addEventListener('click', () => {
-        EditorCore.clearCurrentGrid();
-    });
-
-    document.getElementById('addFrameBtn')?.addEventListener('click', () => {
-        AnimationPlayer.addFrame();
-    });
-
-    document.getElementById('playBtn')?.addEventListener('click', () => {
-        AnimationPlayer.togglePlay();
-    });
+    document.getElementById('clearGridBtn')?.addEventListener('click', () => EditorCore.clearCurrentGrid());
+    document.getElementById('addFrameBtn')?.addEventListener('click', () => AnimationPlayer.addFrame());
+    document.getElementById('playBtn')?.addEventListener('click', () => AnimationPlayer.togglePlay());
 
     document.getElementById('fpsSlider')?.addEventListener('input', (e) => {
-        const target = e.target;
-        document.getElementById('fpsVal').innerText = target.value;
+        document.getElementById('fpsVal').innerText = e.target.value;
         AnimationPlayer.updateSpeed();
+    });
+
+    // Масштабирование (Зум) холста
+    document.getElementById('zoomSlider')?.addEventListener('input', (e) => {
+        const zoom = e.target.value;
+        document.getElementById('zoomVal').innerText = zoom + '%';
+        EditorCore.setZoom(zoom);
+    });
+
+    // Изменение размерности сетки (16, 20, 35 и т.д.)
+    document.getElementById('applyGridSizeBtn')?.addEventListener('click', () => {
+        const newSize = parseInt(document.getElementById('gridSizeInput').value, 10);
+        if (newSize >= 8 && newSize <= 64) {
+            EditorCore.changeGridSize(newSize);
+        }
+    });
+
+    // Применение текстового кода из консоли в пиксели кадра
+    document.getElementById('applyCodeBtn')?.addEventListener('click', () => {
+        AnimationPlayer.loadFrameFromCode();
     });
 });
