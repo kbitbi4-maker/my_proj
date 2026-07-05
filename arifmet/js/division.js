489fe61ddfa8d662f45a63af0c961235299b08f3
kbitbi4-maker <kbitbi4@yandex.ru>
2026-07-05 21:43:44 +0700

    Update division.js

--- arifmet/js/division.js
+++ arifmet/js/division.js
@@ -1,1 +1,32 @@
+// version: v1.0 (Вычислительный движок режима деления)
+import { state } from './state.js';
+import { GameCanvas } from './game_canvas.js';
+import { getMultiplicationHistoryHTML } from './multiplication.js';
+
+export function initDivisionMode() {
+    document.querySelector('.header-menu-btn').innerText = 'Режим: Деление 🍕 ▼';
+    generateDivisionExample();
+}
+
+export function generateDivisionExample() {
+    if (!state.usedExamples) state.usedExamples = [];
+    let d1, d2, result, text;
+    
+    while (true) {
+        d2 = Math.floor(Math.random() * 5) + 2; 
+        result = Math.floor(Math.random() * 5) + 2; 
+        
+        d1 = d2 * result; 
+        text = `${d1}÷${d2}`;
+        
+        if (!state.usedExamples.includes(text)) break;
+    }
+    
+    state.usedExamples.push(text);
+    state.addExample({ exampleText: text, correctValue: result, currentInput: '' });
+
+    GameCanvas.clearZone();
+    GameCanvas.renderHistory(state.examplesHistory, state.activeIndex, state.currentMode, getMultiplicationHistoryHTML);
+    GameCanvas.clearZone();
+}
 
