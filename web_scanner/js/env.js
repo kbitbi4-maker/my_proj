edd861b6dd63f8e4a98037be311bd33c00b1017a
kbitbi4-maker <kbitbi4@yandex.ru>
2026-07-09 23:48:06 +0700

    Delete web_scanner/js directory

--- web_scanner/js/env.js
+++
@@ -1,7 +0,0 @@
-/**
- * Локальные настройки окружения (Файл создается один раз и не перезаписывается AI)
- */
-window.AppEnv = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxWWliIxyk0BxXNE8VriVtLaUbQB31VY8WoAl0hCIoR7fKK_98a70q6C6ioFLlgEofUDw/exec'
-};
-
