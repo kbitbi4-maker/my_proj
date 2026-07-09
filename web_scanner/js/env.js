ccab625b67d98e43695252d594728041427d8a05
kbitbi4-maker <kbitbi4@yandex.ru>
2026-07-09 21:24:47 +0700

    Update env.js

--- web_scanner/js/env.js
+++ web_scanner/js/env.js
@@ -2,6 +2,6 @@
  * Локальные настройки окружения (Файл создается один раз и не перезаписывается AI)
  */
 window.AppEnv = {
-  SCRIPT_URL: 'https://google.com'
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxWWliIxyk0BxXNE8VriVtLaUbQB31VY8WoAl0hCIoR7fKK_98a70q6C6ioFLlgEofUDw/exec'
 };
 
