e1d47158f7d2aa5bc58b9de954a1c9f5297826a7
kbitbi4-maker <kbitbi4@yandex.ru>
2026-07-16 11:16:38 +0700

    Update api.js

--- web_scanner/js/api.js
+++ web_scanner/js/api.js
@@ -1,6 +1,6 @@
 // js/api.js — Модуль сетевого взаимодействия и глобальной фоновой синхронизации
 
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwWSUe23b3fPPnjaOupyCOTS8NL7czcOKC7HdernaDsaOXm9OOYvlE3JH5dR8TLrteUEg/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzL5-olSiESQnkODuWCM7hT2YINeUD355NBFRMd_nxR8Qpwmpsq5dtzbvUa3_-f6xNroA/exec';
 
 window.qrLogs = JSON.parse(localStorage.getItem('qr_db_v9')) || [];
 window.inventoryData = JSON.parse(localStorage.getItem('qr_inventory_v2')) || [];
