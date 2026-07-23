f28ffe33a384a12fceb328a7b151b9a7ce29420f
kbitbi4-maker <kbitbi4@yandex.ru>
2026-07-23 10:47:50 +0700

    Update api.js

--- test_tmc/js/api.js
+++ test_tmc/js/api.js
@@ -1,6 +1,6 @@
 // js/api.js — Модуль сетевого взаимодействия и глобальной фоновой синхронизации
 
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyRK2p5leSQ7ldjMaJTPpgOBtE_qc0SlBppG6Rzw_8wMvXFf6SivtpRPIF3i-YbbQEniw/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxMtvAgQ-3reIyv6MnS0CFmUTpWtpVac7J9-SgZ2qu5Z6ziw-1hKdXQZXUeuicIXGMkcA/exec';
 
 window.qrLogs = JSON.parse(localStorage.getItem('qr_db_v9')) || [];
 window.inventoryData = JSON.parse(localStorage.getItem('qr_inventory_v2')) || [];
