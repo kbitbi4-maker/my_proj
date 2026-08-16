01e45b0e9525a5a6f15b4f16a88f3777d223279c
kbitbi4-maker <kbitbi4@yandex.ru>
2026-08-16 22:05:44 +0700

    Update Engine.js

--- last-spell-clone/src/core/Engine.js
+++ last-spell-clone/src/core/Engine.js
@@ -1,1 +1,37 @@
+export class Engine {
+    constructor() {
+        this.isRunning = false;
+        this.fps = 60;
+        this.lastTime = 0;
+        this.gameLoop = this.gameLoop.bind(this);
+    }
 
+    async init() {
+        // Initialize core engine systems
+        this.isRunning = true;
+        this.gameLoop();
+        return true;
+    }
+
+    gameLoop(timestamp) {
+        if (!this.isRunning) return;
+        
+        const deltaTime = timestamp - this.lastTime;
+        
+        if (deltaTime >= 1000 / this.fps) {
+            this.update(deltaTime);
+            this.lastTime = timestamp;
+        }
+        
+        requestAnimationFrame(this.gameLoop);
+    }
+
+    update(deltaTime) {
+        // Main game update loop
+        // Will be expanded later
+    }
+
+    stop() {
+        this.isRunning = false;
+    }
+}
