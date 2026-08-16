caedf1f78922a21a5cae88e9d314ba1b5afefcbb
kbitbi4-maker <kbitbi4@yandex.ru>
2026-08-16 23:05:41 +0700

    Update AudioManager.js

--- last-spell-clone/src/scripts/audio/AudioManager.js
+++ last-spell-clone/src/scripts/audio/AudioManager.js
@@ -1,1 +1,10 @@
-
+export class AudioManager{
+    constructor(){
+        console.log('AudioManager created');
+    }
+    
+    async init(){
+        console.log('AudioManager init');
+        return true;
+    }
+}
