31e9ab055f37cc51fac8a474eb2e6e038dd2d42b
kbitbi4-maker <kbitbi4@yandex.ru>
2026-08-16 23:05:24 +0700

    Update Engine.js

--- last-spell-clone/src/scripts/core/Engine.js
+++ last-spell-clone/src/scripts/core/Engine.js
@@ -1,1 +1,10 @@
-
+export class Engine{
+    constructor(){
+        console.log('Engine created');
+    }
+    
+    async init(){
+        console.log('Engine init');
+        return true;
+    }
+}
